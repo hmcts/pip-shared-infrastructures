a0ec7c90359dfffda669e2b057ebb1679bacfbb6
Christopher Pateman <christopher.pateman@version1.com>
2021-11-17 08:10:08 +0000

    Update kv name

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 locals {
-  prefix               = "${var.product}-sharedservice-${var.env}"
-  resource_group_name  = "${local.prefix}-rg"
+  prefix               = "${var.product}-sharedservice"
+  resource_group_name  = "${local.prefix}-${var.env}-rg"
   storage_account_name = "${replace(local.prefix, "-", "")}sa"
-  key_vault_name       = "${local.prefix}-kv"
+  key_vault_name       = "${local.prefix}-kv-${var.env}"
   env_long_name        = var.env == "sbox" ? "sandbox" : var.env == "stg" ? "staging" : var.env
 }
 data "azurerm_client_config" "current" {}
