898a35293cb33512c8c0fc5625a3deb7bbbf84a5
Christopher Pateman <christopher.pateman@version1.com>
2021-11-30 10:17:09 +0000

    add env to storage account

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 locals {
   prefix               = "${var.product}-ss"
+  prefix_no_special    = replace(local.prefix, "-", "")
   resource_group_name  = "${local.prefix}-${var.env}-rg"
-  storage_account_name = "${join(",", [replace(local.prefix, "-", ""), var.env])}sa"
+  storage_account_name = "${local.prefix_no_special}sa${var.env}"
   key_vault_name       = "${local.prefix}-kv-${var.env}"
   env_long_name        = var.env == "sbox" ? "sandbox" : var.env == "stg" ? "staging" : var.env
   support_env          = var.env != "prod" ? var.env != "sbox" ? "stg" : "sbox" : var.env
