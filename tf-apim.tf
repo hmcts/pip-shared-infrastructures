9fc2f7eaebc0b9ef97dfc9fc8f56d3c0ce381d37
Christopher Pateman <christopher.pateman@version1.com>
2022-01-10 11:58:03 +0000

    Comment out APIM

--- tf-apim.tf
+++ tf-apim.tf
@@ -3,7 +3,7 @@ locals {
   apim_name = "sds-api-mgmt-${local.env}"
 }
 
-module "apim_product" {
+/* module "apim_product" {
   source                = "git@github.com:hmcts/cnp-module-api-mgmt-product?ref=master"
   api_mgmt_name         = local.apim_name
   api_mgmt_rg           = "ss-${local.env}-network-rg"
@@ -12,4 +12,4 @@ module "apim_product" {
   published             = true
   subscription_required = false
   product_policy        = file("./infrastructure/resources/product-policy.xml")
-} 
+}  */
