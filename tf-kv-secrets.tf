0c9ed2d12d6dd9991e2d6f81e3ab89fda51494b3
Adam Todd <adam.todd@hmcts.net>
2022-02-09 17:52:00 +0000

    Added b2c kv secrets

--- tf-kv-secrets.tf
+++ tf-kv-secrets.tf
@@ -7,6 +7,11 @@ resource "random_password" "session_string" {
   special     = true
 }
 
+data "azuread_domains" "aad_domains" {
+	provider = azuread.otp_sub
+	only_default = true
+}
+
 module "keyvault_secrets" {
   source = "./infrastructure/modules/kv_secrets"
 
@@ -52,7 +57,19 @@ module "keyvault_secrets" {
         "purpose" = "opt-session"
       }
       content_type = ""
-    }
+    },
+	{
+		name = "b2c-auth-endpoint"
+		value = "https://${local.b2c_domain}.b2clogin.com/${local.b2c_domain}.onmicrosoft.com/oauth2/v2.0/authorize"
+		tags = {}
+		content_type = ""
+	},
+	{
+		name = "b2c-token-endpoint"
+		value = "https://${local.b2c_domain}.b2clogin.com/${local.b2c_domain}.onmicrosoft.com/oauth2/v2.0/token"
+		tags = {}
+		content_type = ""
+	}
   ]
 
   depends_on = [
@@ -63,6 +80,7 @@ module "keyvault_secrets" {
 locals {
   bootstrap_prefix  = "${var.product}-bootstrap-${local.support_env}"
   bootstrap_secrets = ["gov-uk-notify-api-key"]
+  b2c_domain = data.azuread_domains.aad_domains.domains.0.domain_name
 }
 data "azurerm_key_vault" "bootstrap_kv" {
   name                = "${local.bootstrap_prefix}-kv"
