fd1efd3a8bce313815eaa206180920d987c3b1a0
Christopher Pateman <christopher.pateman@version1.com>
2022-03-11 11:32:00 +0000

    remove admin consent

--- tf-b2c-apps-concent.tf
+++ tf-b2c-apps-concent.tf
@@ -1,5 +1,12 @@
+/*
 
-# Before giving consent, wait. Sometimes Azure returns a 200, but not all services have access to the newly created applications/services.
+Unable to use Service Principal to grant Admin Consent
+https://docs.microsoft.com/en-us/answers/questions/503886/error-aadsts7000113-when-granting-admin-consent-fo.html
+
+*/
+
+
+/* # Before giving consent, wait. Sometimes Azure returns a 200, but not all services have access to the newly created applications/services.
 resource "null_resource" "delay_before_consent" {
   provisioner "local-exec" {
     command = "sleep 60"
@@ -34,4 +41,4 @@ resource "null_resource" "fe_grant_admin_constent" {
   depends_on = [
     null_resource.delay_before_consent
   ]
-}
+} */
