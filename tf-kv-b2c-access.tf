cd078c620e6c3a667bb834311954441eda1a9871
Christopher Pateman <christopher.pateman@version1.com>
2022-03-17 14:19:29 +0000

    add app kv policy

---
+++ tf-kv-b2c-access.tf
@@ -0,0 +1,15 @@
+resource "azurerm_key_vault_access_policy" "app_mi_access_policy" {
+  key_vault_id = module.kv_apim.key_vault_id
+
+  object_id = data.azurerm_user_assigned_identity.app_mi.object_id
+  tenant_id = data.azurerm_client_config.current.tenant_id
+
+  certificate_permissions = []
+  key_permissions         = []
+  secret_permissions = [
+    "set",
+    "list",
+    "get",
+    "delete",
+  ]
+}
