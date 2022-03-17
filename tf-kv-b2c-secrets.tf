8b27a7dcd830298d2b69d4f2dd3386d175131fe8
Christopher Pateman <christopher.pateman@version1.com>
2022-03-17 13:30:08 +0000

    add tenant id to b2c kv

---
+++ tf-kv-b2c-secrets.tf
@@ -0,0 +1,22 @@
+
+
+module "keyvault_secrets" {
+  source = "./infrastructure/modules/kv_secrets"
+
+  key_vault_id = module.kv_apim.key_vault_id
+  tags         = var.common_tags
+  secrets = [
+    {
+      name  = "b2c-tenant-id"
+      value = var.b2c_tenant_id
+      tags = {
+        "source" : "b2c Tenant"
+      }
+      content_type = ""
+    }
+  ]
+
+  depends_on = [
+    module.kv_apim
+  ]
+}
