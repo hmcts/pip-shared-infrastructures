718da28de2ff66f436079055ebdf8e89296d1dbd
Christopher Pateman <christopher.pateman@version1.com>
2022-03-17 16:15:15 +0000

    correct ID for auth

--- tf-kv-b2c-access.tf
+++ tf-kv-b2c-access.tf
@@ -1,7 +1,7 @@
 resource "azurerm_key_vault_access_policy" "app_mi_access_policy" {
   key_vault_id = module.kv_apim.key_vault_id
 
-  object_id = data.azurerm_user_assigned_identity.app_mi.client_id
+  object_id = data.azurerm_user_assigned_identity.app_mi.principal_id
   tenant_id = data.azurerm_client_config.current.tenant_id
 
   certificate_permissions = []
