8e92fddd93732474ad277787377dc608f2440279
Christopher Pateman <christopher.pateman@version1.com>
2022-03-18 16:23:08 +0000

    Consolidate the Apps

--- tf-b2c-apps-be.tf
+++ tf-b2c-apps-be.tf
@@ -2,14 +2,18 @@ locals {
   be_apps = {
     "account-management" : {
       name          = "account-management"
-      client_access = []
+      client_access = [azuread_application.frontend_apps.*.application_id]
+    },
+    "subscription-management" : {
+      name          = "account-management"
+      client_access = [azuread_application.frontend_apps.*.application_id]
     },
     "data-management" : {
       name = "data-management"
       client_access = concat([
         azuread_application.client_apim_app_hmi.application_id,
         ],
-        azuread_application.frontend_apps.*.application_id
+        [for app in azuread_application.frontend_apps : app.id]
       )
     }
   }
