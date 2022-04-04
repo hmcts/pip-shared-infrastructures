74e3a8118dbe61f111693c27410e59b68f5f0ccf
Christopher Pateman <christopher.pateman@version1.com>
2022-04-04 15:56:50 +0100

    only release apim to stg and sbox

--- tf-apim-main.tf
+++ tf-apim-main.tf
@@ -4,6 +4,7 @@ locals {
 }
 
 module "apim_product" {
+  count                 = local.env == "stg" || local.env == "sbox" ? 1 : 0
   source                = "git@github.com:hmcts/cnp-module-api-mgmt-product?ref=master"
   api_mgmt_name         = local.apim_name
   api_mgmt_rg           = "ss-${local.env}-network-rg"
