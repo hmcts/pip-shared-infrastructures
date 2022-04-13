09a0bb7c5d0e20e542847d692f709929c944a6bb
Christopher Pateman <christopher.pateman@version1.com>
2022-04-13 12:26:28 +0100

    Add apim sub stuff and correct cnp var

--- tf-kv-apim-secrets.tf
+++ tf-kv-apim-secrets.tf
@@ -8,14 +8,14 @@ module "keyvault_apim_secrets" {
   key_vault_id = module.kv_apim.key_vault_id
   tags         = var.common_tags
   secrets = [
-    {
+   /*  {
       name  = "apim-subscription-key"
       value = azurerm_api_management_subscription.product.primary_key
       tags = {
         "source" : local.apim_tag
       }
       content_type = ""
-    }
+    } */
   ]
 
   depends_on = [
