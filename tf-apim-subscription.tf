faa6402c3425d556d92f8ca2af31091b34897fbd
Christopher Pateman <christopher.pateman@version1.com>
2022-05-03 16:49:12 +0100

    update ID

--- tf-apim-subscription.tf
+++ tf-apim-subscription.tf
@@ -3,7 +3,7 @@
 resource "azurerm_api_management_subscription" "product" {
   api_management_name = local.apim_name
   resource_group_name = local.apim_rg
-  product_id          = module.apim_product[0].product_id
+  product_id          = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${local.apim_rg}/providers/Microsoft.ApiManagement/service/${local.apim_name}/products/${module.apim_product[0].product_id}"
   display_name        = "PIP Subscription"
   state               = "active"
   allow_tracing       = var.env == "prod" ? false : true
