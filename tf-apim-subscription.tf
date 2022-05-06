ea7771a79dbd939ef300e3598f536c92210d8460
Christopher Pateman <christopher.pateman@version1.com>
2022-05-06 09:35:08 +0100

    add flag to product and sub

--- tf-apim-subscription.tf
+++ tf-apim-subscription.tf
@@ -1,6 +1,7 @@
 
 
 resource "azurerm_api_management_subscription" "product" {
+  count               = local.env_to_run_in
   api_management_name = local.apim_name
   resource_group_name = local.apim_rg
   product_id          = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${local.apim_rg}/providers/Microsoft.ApiManagement/service/${local.apim_name}/products/${local.apim_product_name}"
