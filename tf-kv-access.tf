0adf70e422dcfe37b9ec9a7e3ac633d45244e55b
Christopher Pateman <christopher.pateman@version1.com>
2022-05-10 10:34:02 +0100

    Add access policy access

--- tf-kv-access.tf
+++ tf-kv-access.tf
@@ -1,9 +1,12 @@
-resource "azurerm_role_assignment" "github_runner" {
-  scope                = module.kv.key_vault_id
-  role_definition_name = "Contributor"
-  principal_id         = var.GITHUB_RUNNER_OBJECT_ID
+resource "azurerm_key_vault_access_policy" "github_access" {
+  key_vault_id = module.kv.key_vault_id
 
-  depends_on = [
-    module.kv
+  object_id = var.GITHUB_RUNNER_OBJECT_ID
+  tenant_id = data.azurerm_client_config.current.tenant_id
+
+  certificate_permissions = []
+  key_permissions         = []
+  secret_permissions = [
+    "Get",
   ]
 }
