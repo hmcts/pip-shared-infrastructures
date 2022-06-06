e0c80d44448b378e7939fa646fade0bfef5d57cd
Christopher Pateman <christopher.pateman@version1.com>
2022-06-06 16:43:57 +0100

    add all urls

--- tf-b2c-main.tf
+++ tf-b2c-main.tf
@@ -11,16 +11,16 @@ resource "random_uuid" "b2c_scope_id" {}
 resource "random_uuid" "client_scope_id" {}
 
 locals {
-  b2c_default_url = (length(regexall("\\.onmircosoft\\.com", data.azuread_domains.b2c_domains.domains.0.domain_name)) > 0)
+  b2c_default_url = (length(regexall("\\.onmircosoft\\.com", local.ad_url)) > 0)
 
-  b2c_domain = replace(replace(data.azuread_domains.b2c_domains.domains.0.domain_name, ".onmicrosoft.com", ".b2clogin.com"), ".co.uk", ".b2clogin.com")
+  b2c_domain = replace(replace(local.ad_url, ".onmicrosoft.com", ".b2clogin.com"), ".co.uk", ".b2clogin.com")
 
   ad_url          = data.azuread_domains.b2c_domains.domains.0.domain_name
   ad_endpoint_url = "https://${local.b2c_domain}/${local.ad_url}"
 
   b2c_urls = [
     local.b2c_domain,
-    "staff.${local.b2c_domain}",
-    "sign-in.${local.b2c_domain}"
+    "staff.${local.ad_url}",
+    "sign-in.${local.ad_url}"
   ]
 }
