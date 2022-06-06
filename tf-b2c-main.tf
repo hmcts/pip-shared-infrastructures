ef211b95e55ade47f44c15bbda1210cb304b8606
Christopher Pateman <christopher.pateman@version1.com>
2022-06-06 16:26:16 +0100

    Add B2C URLs

--- tf-b2c-main.tf
+++ tf-b2c-main.tf
@@ -1,6 +1,6 @@
 data "azuread_domains" "b2c_domains" {
   provider     = azuread.b2c_sub
-  only_default = true
+  only_initial = true
 }
 
 data "azuread_client_config" "ad" {
@@ -11,6 +11,15 @@ resource "random_uuid" "b2c_scope_id" {}
 resource "random_uuid" "client_scope_id" {}
 
 locals {
-  b2c_domain = replace(data.azuread_domains.b2c_domains.domains.0.domain_name, ".onmicrosoft.com", "")
-  b2c_url    = "https://${local.b2c_domain}.b2clogin.com/${local.b2c_domain}.onmicrosoft.com"
+  b2c_default_url = (length(regexall("\\.onmircosoft\\.com", data.azuread_domains.b2c_domains.domains.0.domain_name)) > 0)
+
+  b2c_domain = replace(replace(data.azuread_domains.b2c_domains.domains.0.domain_name, ".onmicrosoft.com", ".b2clogin.com"), ".co.uk", ".b2clogin.com")
+
+  ad_url          = data.azuread_domains.b2c_domains.domains.0.domain_name
+  ad_endpoint_url = "https://${local.b2c_domain}/${local.ad_url}"
+
+  b2c_urls = local.b2c_default_url ? [local.b2c_domain] : [
+    "staff.${local.b2c_domain}",
+    "sign-in.${local.b2c_domain}"
+  ]
 }
