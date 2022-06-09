c2dcccff28c47e962d4c6970c1f704d98ccf339b
Christopher Pateman <christopher.pateman@version1.com>
2022-06-09 16:24:49 +0100

    Correct URLs

--- tf-b2c-main.tf
+++ tf-b2c-main.tf
@@ -14,13 +14,19 @@ locals {
   b2c_default_url = (length(regexall("\\.onmircosoft\\.com", local.ad_url)) > 0)
 
   b2c_domain = replace(replace(local.ad_url, ".onmicrosoft.com", ".b2clogin.com"), ".co.uk", ".b2clogin.com")
+  
+  b2c_staff_endpoint = "staff.${local.ad_url}"
+  b2c_staff_endpoint_url =  var.env == "prod" ? "https://${local.b2c_signin_endpoint}/${local.ad_url}" : local.ad_endpoint_url
 
-  ad_url          = data.azuread_domains.b2c_domains.domains.0.domain_name
+  b2c_signin_endpoint = "sign-in.${local.ad_url}"
+  b2c_signin_endpoint_url =  var.env == "prod" ? "https://${local.b2c_signin_endpoint}/${local.ad_url}" : local.ad_endpoint_url
+
+  ad_url          = var.env == "prod" ? var.domain : data.azuread_domains.b2c_domains.domains.0.domain_name
   ad_endpoint_url = "https://${local.b2c_domain}/${local.ad_url}"
 
   b2c_urls = [
     local.b2c_domain,
-    "staff.${local.ad_url}",
-    "sign-in.${local.ad_url}"
+    local.b2c_staff_endpoint,
+    local.b2c_signin_endpoint
   ]
 }
