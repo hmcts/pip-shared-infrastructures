564ec7ad09056db5f3744897fa574e2f980a88a6
Christopher Pateman <christopher.pateman@version1.com>
2022-06-10 15:55:05 +0100

    replace content on b2c files

--- tf-sa-b2c.tf
+++ tf-sa-b2c.tf
@@ -6,6 +6,12 @@ locals {
     basename(b2c_file_path) => {
       name = basename(b2c_file_path)
       path = "${path.module}/${b2c_file_path}"
+      content = contains(local.image_ext, split(".", b2c_file_path)[1]) ? "" : replace(replace(replace(replace(replace(file("${path.module}/${b2c_file_path}"),
+        "{StorageAccountUrl}", module.sa.storageaccount_primary_blob_endpoint),
+        "{WebsiteUrl}", module.sa.storageaccount_primary_blob_endpoint),
+        "{B2cContainer}", local.b2c_container_name),
+        "{B2cSignInUrl}", local.b2c_signin_endpoint_url),
+      "{B2cStaffUrl}", local.b2c_staff_endpoint_url)
       content_type = (split(".", b2c_file_path)[1] == "css" ? "text/css" :
         split(".", b2c_file_path)[1] == "png" ? "image/png" :
         split(".", b2c_file_path)[1] == "svg" ? "image/svg+xml" :
@@ -15,10 +21,13 @@ locals {
       "")
     }
   }
+
+  image_ext = ["png", "svg", "ico"]
+  file_ext  = ["css", "html", "xml"]
 }
 
-resource "azurerm_storage_blob" "b2c_policy_files" {
-  for_each               = local.b2c_file_details
+resource "azurerm_storage_blob" "b2c_policy_images" {
+  for_each               = { for k, v in local.b2c_file_details : k => v if contains(local.image_ext, split(".", v.path)[1]) }
   name                   = each.value.name
   storage_account_name   = module.sa.storageaccount_name
   storage_container_name = local.b2c_container_name
@@ -31,3 +40,16 @@ resource "azurerm_storage_blob" "b2c_policy_files" {
   ]
 }
 
+resource "azurerm_storage_blob" "b2c_policy_files" {
+  for_each               = { for k, v in local.b2c_file_details : k => v if contains(local.file_ext, split(".", v.path)[1]) }
+  name                   = each.value.name
+  storage_account_name   = module.sa.storageaccount_name
+  storage_container_name = local.b2c_container_name
+  type                   = "Block"
+  source_content         = each.value.path
+  content_type           = each.value.content_type
+
+  depends_on = [
+    module.sa
+  ]
+}
