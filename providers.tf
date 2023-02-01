e6e6ca4c8418ce9424c334b5a84e92ee46f5a335
ChrisS1512 <Christopher.Stacey@justice.gov.uk>
2023-02-01 13:37:19 +0000

    Udpated terraform and azure RM

--- providers.tf
+++ providers.tf
@@ -4,7 +4,7 @@ terraform {
   required_version = ">= 1.0.4"
   required_providers {
     azurerm = {
-      version = ">=2.96.0"
+      version = "3.41.0"
     }
     random = {
       version = ">= 2.2.0"
