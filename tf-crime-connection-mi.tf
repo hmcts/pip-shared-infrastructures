5927952bcb1cc259d34a28478443caf0e059c01f
Josh Blackmoor <joshblackmoor01@icloud.com>
2023-09-26 15:20:18 +0100

    First draft for linking up MIs with crime

---
+++ tf-crime-connection-mi.tf
@@ -0,0 +1,22 @@
+locals {
+  service_account_namespace = "default"
+  service_account_name = "pip-cp-service-account"
+}
+
+data "azurerm_user_assigned_identity" "app_cp_mi" {
+  name                = "${var.product}-cp-${var.env}-mi"
+  resource_group_name = "managed-identities-${var.env}-rg"
+
+  depends_on = [
+    module.kv_cp
+  ]
+}
+
+resource "azurerm_identity_federated_credential" "pip_crime_federated_connection" {
+  name                = "pip-${var.env}-crime-federated-credential"
+  resource_group_name = azurerm_resource_group.rg.name
+  audience            = ["api://AzureADTokenExchange"]
+  issuer              = var.CRIME_AKS_OIDC_ISSUER
+  parent_id           = data.azurerm_user_assigned_identity.app_cp_mi.id
+  subject             = "system:serviceaccount:${var.service_account_namespace}:${var.service_account_name}"
+}
