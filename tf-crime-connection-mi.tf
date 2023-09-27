a06798bbd06245752c2ab164ae70c960739883dd
Josh Blackmoor <joshblackmoor01@icloud.com>
2023-09-27 16:44:27 +0100

    Updated rg for federated identity

--- tf-crime-connection-mi.tf
+++ tf-crime-connection-mi.tf
@@ -1,11 +1,12 @@
 locals {
   service_account_namespace = "default"
   service_account_name      = "pip-${var.env}-cp-service-account"
+  mi_resource_group_name = "managed-identities-${var.env}-rg"
 }
 
 data "azurerm_user_assigned_identity" "app_cp_mi" {
   name                = "${var.product}-cp-${var.env}-mi"
-  resource_group_name = "managed-identities-${var.env}-rg"
+  resource_group_name = local.mi_resource_group_name
 
   depends_on = [
     module.kv_cp
@@ -14,7 +15,7 @@ data "azurerm_user_assigned_identity" "app_cp_mi" {
 
 resource "azurerm_federated_identity_credential" "pip_crime_federated_connection" {
   name                = "pip-${var.env}-crime-federated-credential"
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = local.mi_resource_group_name
   audience            = ["api://AzureADTokenExchange"]
   issuer              = var.CRIME_AKS_OIDC_ISSUER
   parent_id           = data.azurerm_user_assigned_identity.app_cp_mi.id
