0de7225e4f310790d68decb018b3782f3c94383b
Kian Kwa <kian.kwa@justice.gov.uk>
2024-03-05 16:30:25 +0000

    Update alerting settings

--- tf-app-insights-alerting.tf
+++ tf-app-insights-alerting.tf
@@ -14,9 +14,10 @@ module "action-group" {
   env                    = var.env
   resourcegroup_name     = azurerm_resource_group.rg.name
   action_group_name      = "CaTH Email Group"
-  short_name             = "CaTH Group"
+  short_name             = "CaTH-Group"
   email_receiver_name    = "CaTH Email Group"
   email_receiver_address = data.azurerm_key_vault_secret.action-group-email.value
+  count                  = var.env == "prod" ? 1 : 0
 }
 
 module "java-alerting" {
@@ -25,7 +26,7 @@ module "java-alerting" {
 
   app_insights_name = local.java_appinsights_name
 
-  alert_name             = "CaTH Java Exception Alerting"
+  alert_name             = "CaTH-Java-Exception-Alerting"
   alert_desc             = "Triggers when java exceptions occurred within a 15 minutes interval"
   app_insights_query     = "exceptions"
   custom_email_subject   = "Exceptions in CaTH java applications"
@@ -48,7 +49,7 @@ module "nodejs_alerting" {
 
   app_insights_name = local.nodejs_appinsights_name
 
-  alert_name             = "CaTH Nodejs Exception Alerting"
+  alert_name             = "CaTH-Nodejs-Exception-Alerting"
   alert_desc             = "Triggers when nodejs exceptions occurred within a 15 minutes interval"
   app_insights_query     = "exceptions"
   custom_email_subject   = "Exceptions in CaTH nodejs application"
@@ -71,9 +72,8 @@ module "third_party_subscription_alerting" {
 
   app_insights_name = local.java_appinsights_name
 
-  alert_name             = "CaTH Courtel Subscription Error Alerting"
-  alert_desc             = "Triggers when failing to send subscription to third party"
-  app_insights_query     = "requests | where name == \"POST /notify/api\" and resultCode != 200"
+  alert_name             = "CaTH-Courtel-Subscription-Error-Alerting"
+  alert_desc             = "Triggers when failing to send subscription to third party"  app_insights_query     = "requests | where name == \"POST /notify/api\" and resultCode != 200"
   custom_email_subject   = "Error when sending subscription to third party"
   frequency_in_minutes   = local.alert_frequency_in_minutes
   time_window_in_minutes = local.alert_frequency_in_minutes
