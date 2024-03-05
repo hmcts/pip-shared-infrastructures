c08eddb0dd7ec7b359c21d7c40d888cd5ebac8bf
Kian Kwa <kian.kwa@justice.gov.uk>
2024-03-05 13:45:08 +0000

    Update alert names

--- tf-app-insights-alerting.tf
+++ tf-app-insights-alerting.tf
@@ -25,7 +25,7 @@ module "java-alerting" {
 
   app_insights_name = local.java_appinsights_name
 
-  alert_name             = "CaTH Java Exception Alerting"
+  alert_name             = "CaTH-Java-Exception-Alerting"
   alert_desc             = "Triggers when java exceptions occurred within a 15 minutes interval"
   app_insights_query     = "exceptions"
   custom_email_subject   = "Exceptions in CaTH java applications"
@@ -48,7 +48,7 @@ module "nodejs_alerting" {
 
   app_insights_name = local.nodejs_appinsights_name
 
-  alert_name             = "CaTH Nodejs Exception Alerting"
+  alert_name             = "CaTH-Nodejs-Exception-Alerting"
   alert_desc             = "Triggers when nodejs exceptions occurred within a 15 minutes interval"
   app_insights_query     = "exceptions"
   custom_email_subject   = "Exceptions in CaTH nodejs application"
@@ -71,7 +71,7 @@ module "third_party_subscription_alerting" {
 
   app_insights_name = local.java_appinsights_name
 
-  alert_name             = "CaTH Courtel Subscription Error Alerting"
+  alert_name             = "CaTH-Courtel-Subscription-Error-Alerting"
   alert_desc             = "Triggers when failing to send subscription to third party"
   app_insights_query     = "requests | where name == \"POST /notify/api\" and resultCode != 200"
   custom_email_subject   = "Error when sending subscription to third party"
