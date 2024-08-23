b33b001dca6fd80b1c1043d9da9716c973d67cfa
ChrisS1512 <87066931+ChrisS1512@users.noreply.github.com>
2024-08-23 11:15:14 +0100

    Updated Providers

--- providers.tf
+++ providers.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.116.0"
+      version = "3.112.0"
     }
     random = {
       version = ">= 2.2.0"
