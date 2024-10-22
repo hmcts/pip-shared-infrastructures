c641df71ca0bd665ceb5d7df6c86a9065da8abd0
ChrisS1512 <87066931+ChrisS1512@users.noreply.github.com>
2024-10-22 17:10:32 +0100

    PUB-2630 - Updated infra for SSO

--- tf-kv-secrets-bootstrap.tf
+++ tf-kv-secrets-bootstrap.tf
@@ -1,5 +1,39 @@
 locals {
-  bootstrap_secrets = ["gov-uk-notify-api-key", "pip-team-email", "auto-pip-${var.env}-courtel-api", "courtel-certificate", "crime-idam-client-id", "crime-idam-client-secret"]
+  base_bootstrap_secrets = [
+    "gov-uk-notify-api-key",
+    "pip-team-email",
+    "auto-pip-${var.env}-courtel-api",
+    "courtel-certificate",
+    "crime-idam-client-id",
+    "crime-idam-client-secret"
+  ]
+
+  dev_bootstrap_secrets = [
+    "sso-client-id-dev",
+    "sso-client-secret-dev",
+    "sso-config-endpoint-dev",
+    "sso-sg-admin-ctsc-dev",
+    "sso-sg-admin-local-dev",
+    "sso-sg-super-admin-ctsc-dev",
+    "sso-sg-super-admin-local-dev",
+    "sso-sg-system-admin-dev"
+  ]
+
+  //This will no longer be needed, and can be moved up to base_bootstrap_secrets when released to prod
+  sso_bootstrap_secrets = [
+    "sso-client-id",
+    "sso-client-secret",
+    "sso-config-endpoint",
+    "sso-sg-admin-ctsc",
+    "sso-sg-admin-local",
+    "sso-sg-super-admin-ctsc",
+    "sso-sg-super-admin-local",
+    "sso-sg-system-admin"
+  ]
+
+  bootstrap_secrets = var.env == "prod" ? local.base_bootstrap_secrets :
+    var.env == "stg" ? concat(local.base_bootstrap_secrets, local.dev_bootstrap_secrets, local.sso_bootstrap_secrets) :
+      concat(local.base_bootstrap_secrets, local.sso_bootstrap_secrets)
 }
 
 data "azurerm_key_vault_secret" "bootstrap_secrets" {
