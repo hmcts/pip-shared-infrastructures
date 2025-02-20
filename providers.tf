ff6533a58362e95e71f0623710306b6638076d95
KianKwa <80688761+KianKwa@users.noreply.github.com>
2025-02-20 10:03:21 +0000

    Monthly dependency update (#212)

--- providers.tf
+++ providers.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.15.0"
+      version = "4.18.0"
     }
     random = {
       version = ">= 2.2.0"
