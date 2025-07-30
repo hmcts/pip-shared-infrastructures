04d3c03ec91494f3237007756f8ed5cb8ea99f60
ChrisS1512 <87066931+ChrisS1512@users.noreply.github.com>
2025-07-30 13:08:09 +0100

    PUB-2959 - Dependency Updates (#229)

--- providers.tf
+++ providers.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.33.0"
+      version = "4.37.0"
     }
     random = {
       version = ">= 2.2.0"
@@ -15,7 +15,7 @@ terraform {
     }
     azapi = {
       source  = "Azure/azapi"
-      version = "~> 1.15.0"
+      version = "~> 2.5.0"
     }
   }
 }
