692a4da46d0459687e4381feaa4a86ceb101cf89
Christopher Stacey <christopher.stacey@justice.gov.uk>
2025-08-13 12:44:34 +0100

    PUB-2365 - Removed admin URLs

--- tf-b2c-main.tf
+++ tf-b2c-main.tf
@@ -15,9 +15,6 @@ locals {
 
   b2c_domain = replace(replace(local.ad_url, ".onmicrosoft.com", ".b2clogin.com"), ".service.gov.uk", ".b2clogin.com")
 
-  b2c_staff_endpoint     = var.env != "prod" ? "staff.pip-frontend.${var.domain}" : "staff.${local.ad_url}"
-  b2c_staff_endpoint_url = var.env == "prod" ? "https://${local.b2c_staff_endpoint}/${local.ad_url}" : local.ad_endpoint_url
-
   b2c_signin_endpoint     = var.env != "prod" ? "sign-in.pip-frontend.${var.domain}" : "sign-in.${local.ad_url}"
   b2c_signin_endpoint_url = var.env == "prod" ? "https://${local.b2c_signin_endpoint}/${local.ad_url}" : local.ad_endpoint_url
 
@@ -27,14 +24,12 @@ locals {
   //This specific logic for staging is needed due to the pages in the user flows only pointing to Staging blob store.
   b2c_urls = var.env == "stg" ? [
     "https://${local.b2c_domain}",
-    "https://${local.b2c_staff_endpoint}",
     "https://${local.b2c_signin_endpoint}",
     "https://*.pip-frontend.test.platform.hmcts.net",
     "https://*.pip-frontend.demo.platform.hmcts.net",
     "https://*.pip-frontend.ithc.platform.hmcts.net",
     ] : [
     "https://${local.b2c_domain}",
-    "https://${local.b2c_staff_endpoint}",
     "https://${local.b2c_signin_endpoint}"
   ]
 }
