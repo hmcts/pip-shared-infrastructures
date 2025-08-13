4244218ea25b6a19a7a2259c8069900c790e8b6f
Christopher Stacey <christopher.stacey@justice.gov.uk>
2025-08-13 16:53:04 +0100

    PUB-2985 - Fixed conflicts

--- tf-kv-bootstrap.tf
+++ tf-kv-bootstrap.tf
@@ -1,18 +1,18 @@
 locals {
-  prefix              = "${var.product}-bootstrap-${var.env}"
-  resource_group_name = "${local.prefix}-rg"
-  key_vault_name      = "${local.prefix}-kv"
+  bootstrap_prefix              = "${var.product}-bootstrap-${var.env}"
+  bootstrap_resource_group_name = "${local.prefix}-rg"
+  bootstrap_key_vault_name      = "${local.prefix}-kv"
 }
 
 resource "azurerm_resource_group" "bootstrap_rg" {
-  name     = local.resource_group_name
+  name     = local.bootstrap_resource_group_name
   location = var.location
   tags     = var.common_tags
 }
 
 module "boostrap_kv" {
   source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
-  name                    = local.key_vault_name
+  name                    = local.bootstrap_key_vault_name
   product                 = var.product
   env                     = var.env
   object_id               = "7ef3b6ce-3974-41ab-8512-c3ef4bb8ae01"
