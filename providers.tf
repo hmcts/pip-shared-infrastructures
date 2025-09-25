cde8834f308a20861203dc5e39f78f020a11028e
ChrisS1512 <87066931+ChrisS1512@users.noreply.github.com>
2025-09-25 13:18:05 +0100

    PUB-3000 - Dependency Updates (#239)
    * PUB-3000 - Dependency Updates
    
    * Updated provider
    
    * Updated provider
    
    * Updated provider

--- providers.tf
+++ providers.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.37.0"
+      version = "4.42.0"
     }
     random = {
       version = ">= 2.2.0"
