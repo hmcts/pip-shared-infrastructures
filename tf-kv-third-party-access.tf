5e3d9c19d38e87a088707171e5b2f049ce7fca60
junaidiqbalmoj <84805836+junaidiqbalmoj@users.noreply.github.com>
2026-02-11 10:31:35 +0000

    fix build

--- tf-kv-third-party-access.tf
+++ tf-kv-third-party-access.tf
@@ -1,5 +1,5 @@
 resource "azurerm_key_vault_access_policy" "cath_mi_access_policy" {
-  key_vault_id = module.kv_third_party.key_vault_id
+  key_vault_id = module.kv_third_party[0].key_vault_id
 
   object_id = azurerm_user_assigned_identity.cath-mi[0].principal_id
   tenant_id = data.azurerm_client_config.current.tenant_id
