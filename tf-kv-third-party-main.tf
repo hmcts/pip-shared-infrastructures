e3a2436b84c770fa22d5c686afd4f2c80547be79
junaidiqbalmoj <84805836+junaidiqbalmoj@users.noreply.github.com>
2026-02-13 16:34:39 +0000

    Fix secrets issue

--- tf-kv-third-party-main.tf
+++ tf-kv-third-party-main.tf
@@ -17,7 +17,7 @@ module "kv_third_party" {
 }
 
 resource "azurerm_key_vault_secret" "cath_mi_client_id" {
-  name         = "cath-mi-id"
+  name         = "cath-mi-client-id"
   value        = azurerm_user_assigned_identity.cath-mi[0].client_id
   key_vault_id = module.kv.key_vault_id
   count        = var.env == "prod" ? 0 : 1
