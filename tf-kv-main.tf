76b0dec4deb5b6918527491a81ca9ce4b49d5f23
Daniel Alves <daniel.alves@justice.gov.uk>
2026-07-20 16:30:02 +0100

    fix: grant Jenkins access to shared key vaults

--- tf-kv-main.tf
+++ tf-kv-main.tf
@@ -4,17 +4,18 @@ locals {
 }
 
 module "kv" {
-  source = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
+  source = "git@github.com:hmcts/cnp-module-key-vault?ref=DTSPO-31965/remove-jenkins-ptl-access"
 
-  name                    = local.key_vault_name
-  product                 = var.product
-  env                     = var.env
-  object_id               = "7ef3b6ce-3974-41ab-8512-c3ef4bb8ae01"
-  jenkins_object_id       = data.azurerm_user_assigned_identity.jenkins.principal_id
-  resource_group_name     = azurerm_resource_group.rg.name
-  product_group_name      = var.active_directory_group
-  common_tags             = var.common_tags
-  create_managed_identity = true
+  name                     = local.key_vault_name
+  product                  = var.product
+  env                      = var.env
+  object_id                = "7ef3b6ce-3974-41ab-8512-c3ef4bb8ae01"
+  jenkins_object_id        = data.azurerm_user_assigned_identity.jenkins.principal_id
+  grant_dev_jenkins_access = var.env == "stg"
+  resource_group_name      = azurerm_resource_group.rg.name
+  product_group_name       = var.active_directory_group
+  common_tags              = var.common_tags
+  create_managed_identity  = true
 }
 
 data "azurerm_key_vault" "bootstrap_kv" {
